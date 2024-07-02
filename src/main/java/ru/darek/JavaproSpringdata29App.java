51e2991b26f02c78057dfbb02d5293bbb663aed1
Darek <darek68@list.ru>
2024-07-02 19:41:26 +0300

    MasterCommitFirst

---
+++ src/main/java/ru/darek/JavaproSpringdata29App.java
@@ -0,0 +1,13 @@
+package ru.darek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaproSpringdata29App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaproSpringdata29App.class, args);
+	}
+
+}
